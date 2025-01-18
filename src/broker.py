7b5e784182d0fcd17b553ea077a67ecaaae4cd0a
Jhon <jhongil96@gmail.com>
2025-01-17 23:59:54 -0500

    basename of file

--- src/broker.py
+++ src/broker.py
@@ -8,7 +8,7 @@ async def main():
 
         #data = {"text": "Quiero construir un audio muchisimo más largo, entonces voy a enviar un texto bien largo para explicar esta vaina. Cuando haga el audio luego debo subir el audio a S3 que posteriormente será descargado por el servcio de video y procesará el resto de cosas necesarias para formar un video completo.", "speaker": "speakers/herrera.wav"}
         
-        data = {"id": 1, "text": "Ducati es reconocida mundialmente como un símbolo de lujo y alto rendimiento en el mundo de las motocicletas. Su diseño italiano impecable, combinado con tecnologías innovadoras como la distribución desmodrómica, ha establecido estándares en el segmento de las motos deportivas y de lujo. Modelos como la Panigale y la Streetfighter no solo destacan por su potencia y precisión en el manejo", "speaker": "daniel"}
+        data = {"id": 1, "text": "Ducati es reconocida mundialmente como un símbolo de lujo y alto rendimiento en el mundo de las motocicletas. Su diseño italiano impecable, combinado con tecnologías innovadoras como la distribución desmodrómica, ha establecido estándares en el segmento de las motos deportivas y de lujo.", "speaker": "daniel"}
         await nc.publish("job.tts.created", json.dumps(data).encode())
         await nc.drain()
     except Exception as e:
--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ class Main:
         utils.print_info(f"Received a message on '{msg.subject}': {msg.data.decode()}")
         data = json.loads(msg.data.decode())
         output = self._tts.synthesize(data['text'], f"speakers/{data['speaker']}.wav")
-        filename = output.split('\\')[-1]
+        filename = os.path.basename(output)
         key = f"audio/{data['id']}/{filename}"
         self._s3.upload_file(output, get_s3_bucket(), key)
         os.remove(output)
